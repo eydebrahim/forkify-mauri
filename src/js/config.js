a46bb27a0c7e446df1db39723ccc8fe348b00649
eyde brahim <eydebrahim@gmail.com>
2022-08-03 16:18:55 +0000

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6dec4fdc-0429-4071-b159-686a6225d580';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/View.js
@@ -0,0 +1,104 @@
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+export default class View {
+  _data;
+
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[] } data the data to be redered (e.g recipe)
+   * @param {boolean} [render=true] if false create markUp string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author EydeBrahim
+   * @todo Finish implementation
+   */
+  render(data, render = true) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+
+    this._data = data;
+    const markUp = this._generateMarkup();
+    if (!render) return markUp;
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markUp);
+  }
+  update(data) {
+    this._data = data;
+    const newMarkUp = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkUp);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+
+      // Updates changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Updates changed ATTRIBUTE
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+    });
+  }
+
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
+
+  renderSpinner() {
+    const markUp = `
+      <div class="spinner">
+         <svg>
+           <use href="${icons}#icon-loader"></use>
+        </svg>
+       </div>
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markUp);
+  }
+
+  renderError(message = this._errorMessage) {
+    const markUp = `
+    <div class="error">
+     <div>
+       <svg>
+        <use href="${icons}#icon-alert-triangle"></use>
+       </svg>
+     </div>
+     <p>${message}</p>
+    </div>
+    
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markUp);
+  }
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
+  }
+
+  renderMessage(message = this._message) {
+    const markUp = `
+    <div class="message">
+     <div>
+       <svg>
+        <use href="${icons}#icon-smile"></use>
+       </svg>
+     </div>
+     <p>${message}</p>
+    </div>
+    
+    `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markUp);
+  }
+}
